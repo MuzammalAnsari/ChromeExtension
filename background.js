e31f9a7bd8d6f6d58450178dd26ba712b991226a
Muzammal Ansari <muzammalbash@gmail.com>
2024-08-26 15:07:12 +0500

    learned about tabs and its different events

--- background.js
+++ background.js
@@ -27,12 +27,12 @@ chrome.contextMenus.create({
     title: 'Copy 1',
     contexts: ['all']
 }) 
-//remove
 
+
+// remove
 chrome.contextMenus.remove('copy 1')
 
-chrome.contextMenus.onClicked.addListener(
-    function (info, tab) {
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
         console.log(info);
         console.log(tab);
         
@@ -42,6 +42,31 @@ chrome.contextMenus.onClicked.addListener(
         //         func: copyText
         //     })
         // }
-    }
-    
-  )
+})
+
+
+//   chrome.runtime.onInstalled.addListener(()=>{
+//     // chrome.tabs.create({
+//     //     url: 'https://www.google.com'
+//     // })
+
+//     chrome.tabs.query({active: true, currentWindow: true}, (tabs) =>{
+//         console.log(tabs[0])
+//     })
+// })
+
+
+// chrome.tabs.onCreated.addListener((tab)=>{
+//     console.log('tab created'),
+//     console.log(tab)
+//     // chrome.tabs.sendMessage(tab.id, {message: 'new tab created'})
+// })
+
+//tab update
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab)=>{
+    console.log('tab updated'),
+    console.log(tabId)
+    console.log(changeInfo)
+    console.log(tab)
+    // chrome.tabs.sendMessage(tab.id, {message: 'new tab updated'})
+})
