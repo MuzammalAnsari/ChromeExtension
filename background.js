d3a1bec9d4dd30d46bafce18e41c7b210ddd742e
Muzammal Ansari <muzammalbash@gmail.com>
2024-08-26 14:25:38 +0500

    learned about contextMenus

--- background.js
+++ background.js
@@ -9,9 +9,39 @@
 
 
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log(request);
-    console.log(sender);
-    console.log(sendResponse);
-    sendResponse({farewell: 'goodbye'});
-})
+// chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+//     console.log(request);
+//     console.log(sender);
+//     console.log(sendResponse);
+//     sendResponse({farewell: 'goodbye'});
+// })
+
+chrome.contextMenus.create({
+    id: 'copy',
+    title: 'Copy',
+    contexts: ['all']
+}) 
+
+chrome.contextMenus.create({
+    id: 'copy 1',
+    title: 'Copy 1',
+    contexts: ['all']
+}) 
+//remove
+
+chrome.contextMenus.remove('copy 1')
+
+chrome.contextMenus.onClicked.addListener(
+    function (info, tab) {
+        console.log(info);
+        console.log(tab);
+        
+        // if (info.menuItemId === 'copy') {
+        //     chrome.scripting.executeScript({
+        //         target: {tabId: tab.id},
+        //         func: copyText
+        //     })
+        // }
+    }
+    
+  )
--- popup.js
+++ popup.js
@@ -1,10 +1,10 @@
 alert('hello World')
 // console.log(this);
 
-chrome.action.setBadgeText({text: 'ON'});
+// chrome.action.setBadgeText({text: 'ON'});
 
-chrome.action.setBadgeTextColor({color: '#fff'})
+// chrome.action.setBadgeTextColor({color: '#fff'})
 
-chrome.action.getBadgeText({}, (result) => {
-  console.log(result)
-});
+// chrome.action.getBadgeText({}, (result) => {
+//   console.log(result)
+// });
