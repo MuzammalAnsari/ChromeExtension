cedac477e9bb53d3438faaf8e401998ed751b5e4
Muzammal Ansari <muzammalbash@gmail.com>
2024-08-26 10:48:42 +0500

    learned about chrome.action and created a notification on extension icon when clicked

--- popup.js
+++ popup.js
@@ -1,1 +1,10 @@
 alert('hello World')
+// console.log(this);
+
+chrome.action.setBadgeText({text: 'ON'});
+
+chrome.action.setBadgeTextColor({color: '#fff'})
+
+chrome.action.getBadgeText({}, (result) => {
+  console.log(result)
+});
