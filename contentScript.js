1c93091f2057d6bb7ef43db1a3065e2edf3a3f38
Muzammal Ansari <muzammalbash@gmail.com>
2024-08-26 10:14:01 +0500

    learned about storage and permissions

--- contentScript.js
+++ contentScript.js
@@ -1,4 +1,20 @@
 // alert("im from contentScript")
-chrome.runtime.sendMessage({greetings: "hello"}, function(response){
-    console.log(response.farewell);
+
+
+// chrome.runtime.sendMessage({greetings: "hello"}, function(response){
+//     console.log(response.farewell);
+// })
+
+
+//local storage extension get
+chrome.storage.local.set({'name': 'ansari'}).then(()=>{
+    console.log("value set")
 })
+
+chrome.storage.local.get(['name']).then((result)=>{
+    console.log("value currently is " + result.name)
+});
+
+//local storage extension set
+
+
